4f36c7b62f53ecba5b813c61f0f88064df4792ca
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2022-01-22 15:26:56 +0000

    Finished TP11

--- aed2122_p11/aed2122_p11/Tests/graph.cpp
+++ aed2122_p11/aed2122_p11/Tests/graph.cpp
@@ -4,6 +4,8 @@
 #include "graph.h"
 #include <climits>
 
+#define INF (INT_MAX/2)
+
 // Constructor: nr nodes and direction (default: undirected)
 Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {
 }
@@ -23,31 +25,38 @@ void Graph::addEdge(int src, int dest, int weight) {
 // ..............................
 // a) Distância entre dois nós
 // TODO
-int Graph::dijkstra_distance(int a, int b) {
 
-    MinHeap<int, int> minHeap(nodes.size(), -1);
-
-    for (int i = 1; i < nodes.size(); i++) {
-        nodes[i].dist = INT_MAX;
+void Graph::dijkstra(int s) {
+    MinHeap<int, int> minHeap(n, -1);
+    for (int i = 1; i <= n; i++) {
+        nodes[i].dist = INF;
         nodes[i].visited = false;
-        minHeap.insert(i, nodes[i].dist);
+        minHeap.insert(i, INF);
     }
-    nodes[a].dist = 0;
-    minHeap.decreaseKey(a, 0);
-    while (minHeap.getSize() != 0) {
+    nodes[s].dist = 0;
+    nodes[s].pred = s;
+    minHeap.decreaseKey(s,0);
+
+    while (minHeap.getSize() > 0) {
         int u = minHeap.removeMin();
-        if (!nodes[u].visited) {
-            nodes[u].visited = true;
-            for (auto it = nodes[u].adj.begin(); it != nodes[u].adj.end(); it++) {
-                if (!nodes[(*it).dest].visited && nodes[(*it).dest].dist > nodes[u].dist + (*it).weight) {
-                    nodes[(*it).dest].dist = nodes[u].dist + (*it).weight;
-                    minHeap.decreaseKey((*it).dest, nodes[u].dist + (*it).weight);
-                }
+        nodes[u].visited = true;
+        for (const auto & edge : nodes[u].adj) {
+            int v = edge.dest;
+            int w = edge.weight;
+            if (!nodes[v].visited && nodes[u].dist + w < nodes[v].dist) {
+                nodes[v].dist = nodes[u].dist + w;
+                minHeap.decreaseKey(v, nodes[v].dist);
+                nodes[v].pred = u;
             }
         }
 
     }
-    if (nodes[b].dist == INT_MAX) return -1;
+}
+
+int Graph::dijkstra_distance(int a, int b) {
+
+    dijkstra(a);
+    if (nodes[b].dist == INF) return -1;
     return nodes[b].dist;
 }
 
@@ -55,43 +64,16 @@ int Graph::dijkstra_distance(int a, int b) {
 // b) Caminho mais curto entre dois nós
 // TODO
 list<int> Graph::dijkstra_path(int a, int b) {
+
+    dijkstra(a);
     list<int> path;
 
-    MinHeap<int, int> minHeap(nodes.size(), -1);
-
-    for (int i = 1; i < nodes.size(); i++) {
-        nodes[i].dist = INT_MAX;
-        nodes[i].visited = false;
-        nodes[i].pred = 0;
-        minHeap.insert(i, nodes[i].dist);
+    if (nodes[b].dist == INF) return path;
+    path.push_back(b);
+    int v = b;
+    while (v != a) {
+        v = nodes[v].pred;
+        path.push_front(v);
     }
-    nodes[a].dist = 0;
-    nodes[a].pred = a;
-    minHeap.decreaseKey(a, 0);
-    while (minHeap.getSize() != 0) {
-        int u = minHeap.removeMin();
-        if (!nodes[u].visited) {
-            nodes[u].visited = true;
-            for (auto it = nodes[u].adj.begin(); it != nodes[u].adj.end(); it++) {
-                if (!nodes[(*it).dest].visited && nodes[(*it).dest].dist > nodes[u].dist + (*it).weight) {
-                    nodes[(*it).dest].dist = nodes[u].dist + (*it).weight;
-                    nodes[(*it).dest].pred = u;
-                    minHeap.decreaseKey((*it).dest, nodes[u].dist + (*it).weight);
-                }
-            }
-        }
-
-    }
-    int i = b;
-
-    if (nodes[b].dist == INT_MAX) return path;
-
-    while (i != a) {
-        path.push_front(i);
-        i = nodes[i].pred;
-    }
-
-    path.push_front(a);
-
     return path;
 }
